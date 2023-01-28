a0ae0174e24d274836d5cd297db4b625a1b5204a
Jahnavi Nandanwar <77223541+JD771@users.noreply.github.com>
2023-01-28 15:41:39 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,17 +1,14 @@
-# x = 10
-# y = 20
+x = 10
+y = 20
 
-# sum = x+ y
-# difference = x-y
-# multiplication = x*y
-# division = y // x
-# remainder = y % x
+sum = x+ y
+difference = x-y
+multiplication = x*y
+division = y // x
+remainder = y % x
 
-# print('The sum of x and y is: ', sum)
-# print('The difference of x and y is:', difference)
-# print('The multiplication of x and y is: ', multiplication)
-# print('The division of x and y is:', division)
-# print('The remainder of x and y is:', remainder)
-
-List = [1,2,3]
-print(List)
+print('The sum of x and y is: ', sum)
+print('The difference of x and y is:', difference)
+print('The multiplication of x and y is: ', multiplication)
+print('The division of x and y is:', division)
+print('The remainder of x and y is:', remainder)
