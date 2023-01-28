5f60033bb4bc51e3a0ad5dbc0083ac81c1cb4aaf
Jahnavi Nandanwar <77223541+JD771@users.noreply.github.com>
2023-01-28 15:19:58 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,17 @@
+# x = 10
+# y = 20
+
+# sum = x+ y
+# difference = x-y
+# multiplication = x*y
+# division = y // x
+# remainder = y % x
+
+# print('The sum of x and y is: ', sum)
+# print('The difference of x and y is:', difference)
+# print('The multiplication of x and y is: ', multiplication)
+# print('The division of x and y is:', division)
+# print('The remainder of x and y is:', remainder)
+
+List = [1,2,3]
+print(List)
